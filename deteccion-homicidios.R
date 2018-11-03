59a56c50231687b62333f3b9a9ec4063bbda1087
Felipe Gonzalez <felipexgonzalez@gmail.com>
2018-11-02 19:47:43 -0600

    Prototipo de modelo simple bayesiano

---
+++ deteccion-homicidios.R
@@ -0,0 +1,44 @@
+simple_cap <- function(x) {
+    s <- strsplit(tolower(x), " ")[[1]]
+    paste(toupper(substring(s, 1, 1)), substring(s, 2),
+          sep = "", collapse = " ")
+}
+
+monitor <- function(hom_df, periods = 10, alpha = 0.02){
+    hom_df <- arrange(hom_df, date)
+    
+    observed <- hom_df$count
+    n <- length(observed)
+    range <- (n - periods + 1):n
+    obs_dprog <- ts(observed, start = c(2015,1), frequency = 12) %>% 
+        as("sts") %>% sts2disProg
+    
+    monitor_surv <- algo.bayes(obs_dprog, 
+                               control = list(range = range, b = 0, w = 6, alpha = alpha))
+    hom_df$alarm <- NA
+    hom_df$upper_bound <- NA
+    hom_df$alarm[range] <- monitor_surv$alarm
+    hom_df$upper_bound[range] <- monitor_surv$upperbound
+    list(monitor = monitor_surv, data = hom_df)
+}
+
+graf_monitor <- function(monitor){
+    data <- monitor$data
+    state <- first(data$state) %>% simple_cap
+    mun <- first(data$municipio) %>% simple_cap
+    graf <- ggplot(data, aes(x = date, y = count)) + 
+        geom_ribbon(aes(ymin=0, ymax = upper_bound) ,alpha = 0.5, fill="gray80") +
+        geom_line(aes(y = count), colour='gray') +
+        geom_point(aes(y = count), colour='gray') +
+        scale_colour_manual(values = c("black", "red")) +
+        scale_size(range=c(2,4)) +
+        theme_minimal() + guides(colour=FALSE) + guides(size = FALSE) +
+        ylab("Número de homicidios") +
+        labs(title = paste0(mun, ", ", state))
+    if(max(data$alarm, na.rm = TRUE) == 1){
+        graf <- graf + geom_point(aes(y = count, colour = factor(alarm), size=alarm)) 
+    } else {
+        graf <- graf + geom_point(aes(y = count, colour = factor(alarm)))
+    }
+    print(graf)
+}
